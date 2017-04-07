b48c64cd313683f583abc59ac2d49e74a7914870
Paul Poot <paul@paulpoot.eu>
2017-04-07 12:36:42 +0200

    Added error handling and improved button animation. Token is now stored.

--- src/components/LoginForm/loginForm.js
+++ src/components/LoginForm/loginForm.js
@@ -14,11 +14,13 @@ export default Vue.extend({
     };
     var loginText = 'Login';
     var loggingIn = false;
+    var error = false;
 
     return {
       user: user,
       loginText: loginText,
       loggingIn: loggingIn,
+      error: error,
     };
   },
   methods: {
@@ -26,38 +28,74 @@ export default Vue.extend({
       this.loginText = loginText;
       this.loggingIn = loggingIn;
     },
+
     toPage: function(route) {
       this.$router.push(route);
     },
-    login: function(e) {
+
+    loginStart: function(e) {
       this.updateButton('Logging in...', true);
 
       Velocity(e.target, {
         marginBottom: 0,
-        marginLeft: 0,
         paddingTop: '24px',
         paddingBottom: '24px',
-        width: '100%',
         boxShadowY: 0,
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+      }, {
+        duration: 200,
+        easing: 'easeInQuad',
+        complete: this.login(e),
+      });
+      Velocity(e.target.parentElement, {
+        paddingLeft: 0,
+        paddingRight: 0,
       }, {
         duration: 200,
         easing: 'easeInQuad',
       });
+    },
 
+    login: function(e) {
       axios.post(API_BASE + '/token', this.user)
-       .then(response => {
-         console.log(response);
+        .then(response => {
+          this.$store.commit('updateToken', response.data);
 
-         axios.get(API_BASE + '/users', {
-           headers: { 'Authorization': response.data }
-         })
-          .then(response2 => {
-            console.log(response2);
-          });
-       })
-       .catch(response => {
-         console.log(response);
-       });
+          axios.get(API_BASE + '/users', {
+            headers: { 'Authorization': response.data }
+          })
+            .then(response => {
+              console.log(response);
+            })
+            .catch(error => {
+              var self = this;
+              this.handleError(self, e, error);
+            });
+        })
+        .catch(error => {
+          var self = this;
+          this.handleError(self, e, error);
+        });
+    },
+    handleError(self, e, error) {
+      console.log(error);
+
+      Velocity(e.target, 'reverse', {
+        delay: 1000,
+        duration: 200,
+        easing: 'easeInQuad',
+      });
+      Velocity(e.target.parentElement, 'reverse', {
+        delay: 1000,
+        duration: 200,
+        easing: 'easeInQuad',
+        complete: function() {
+          self.error = 'We could not log you in using these credentials.';
+          self.loggingIn = false;
+          self.loginText = 'Login';
+        }
+      });
     }
   },
 });
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 import VueRouter from 'vue-router';
-
 import { LoadingState } from 'src/config/loading-state';
 import Navigation from 'components/Navigation/navigation';
 import Loader from 'components/Loader/Loader';
+import { store } from './util/store';
 
+Vue.use(Vuex);
 Vue.use(VueRouter);
 
 import routes from 'src/routes';
@@ -13,10 +16,21 @@ import 'src/style.scss';
 export const router = new VueRouter({
   routes,
   mode: 'history',
-  linkActiveClass: 'active'
+  linkActiveClass: 'active',
+  computed: {
+    ...mapGetters([
+      'token'
+    ])
+  },
+  methods: {
+    ...mapMutations([
+      'updateToken'
+    ])
+  }
 });
 
 new Vue({
+  store,
   router,
   components: {
     Navigation,
